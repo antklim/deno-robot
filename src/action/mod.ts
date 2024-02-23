142fd60e88dacc66a8e8f88d36c4447ae9e653bb
antklim <antklim@gmail.com>
2024-02-23 13:38:07 +1100

    refactor: actions

--- src/action/mod.ts
+++ src/action/mod.ts
@@ -1,3 +1,28 @@
-export * from "./move.ts";
-export * from "./place.ts";
-export * from "./turn.ts";
+import { move, MoveProps } from "./move.ts";
+import { place, PlaceProps } from "./place.ts";
+import { turn, TurnProps } from "./turn.ts";
+
+export type Action =
+  | {
+    type: "PLACE";
+    props: PlaceProps;
+  }
+  | {
+    type: "MOVE";
+    props: MoveProps;
+  }
+  | {
+    type: "TURN";
+    props: TurnProps;
+  };
+
+export function action(action: Action) {
+  switch (action.type) {
+    case "PLACE":
+      return place(action.props);
+    case "MOVE":
+      return move(action.props);
+    case "TURN":
+      return turn(action.props);
+  }
+}
