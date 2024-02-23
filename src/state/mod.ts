56094ead5041932bf5df3609179306e64a580a0e
antklim <antklim@gmail.com>
2024-02-23 14:09:40 +1100

    feat: added state setter and getter

--- src/state/mod.ts
+++ src/state/mod.ts
@@ -4,3 +4,15 @@ export interface State {
   pos: Position;
   dir: Direction;
 }
+
+let state: State | null = null;
+
+export function set({ pos, dir }: State): void {
+  state = { pos: { ...pos }, dir };
+}
+
+export function get(): State | null {
+  if (state == null) return null;
+
+  return { pos: { ...state.pos }, dir: state.dir };
+}
